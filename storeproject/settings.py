59a1d3d499fcfc24ba52bda11e91c0bb334379ca
Abrahamjohnson <123575149+Abrahamjohnson@users.noreply.github.com>
2025-10-12 23:15:24 +0530

    Update settings.py

--- storeproject/settings.py
+++ storeproject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(kehhhum13ms!#&4%_qlj%imh83e^^ihl*=3ss!b#s3*w=%)y*'
